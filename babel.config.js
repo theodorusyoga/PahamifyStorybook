7b58c4a3dcba3335a62673b111287dc53ff9f57d
Theodorus <theoyoga@outlook.com>
2021-03-23 00:46:46 +0700

    Init RN + storybook + babel module resolver

---
+++ babel.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: ['.ios.js', '.android.js', '.js', '.json'],
+        root: ['./src'],
+      },
+    ],
+  ],
+};
